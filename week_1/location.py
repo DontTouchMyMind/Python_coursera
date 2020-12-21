b26ba8788a01372df28ef19410735d2db644a985
DontTouchMyMind <tobigface@gmail.com>
2020-12-21 15:50:05 +0200

    Code review.

--- week_1/location.py
+++ week_1/location.py
@@ -6,12 +6,5 @@ def get_location_info():
     return requests.get('https://freegeoip.app/json/').json()
 
 
-
 if __name__ == '__main__':
     pprint.pprint(get_location_info())
-
-    p = 'привет'
-    print(p[0:1])
-    pi = 3.1415926
-    pi_fmt = f"{pi:#0.2f}"
-    print(type(pi_fmt))
