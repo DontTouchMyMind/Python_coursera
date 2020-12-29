97b72834624b15da688d49d77a73f22836f91533
DontTouchMyMind <tobigface@gmail.com>
2020-12-29 09:42:31 +0200

    Need to be improved.

--- week_2/storage.py
+++ week_2/storage.py
@@ -9,45 +9,29 @@ parser.add_argument('--key', help='Key help')
 parser.add_argument('--val', help='Value help')
 args = parser.parse_args()
 
-storage_path = os.path.join(tempfile.gettempdir(), 'storage18.data')
+storage_path = os.path.join(tempfile.gettempdir(), 'storage.data')
 
-if not os.path.isfile(storage_path):
-    with open(storage_path, 'w') as file_create:
-        data = {args.key: [args.val]}
-        json.dump(data, file_create)
-        print('file was created')
-else:
-    with open(storage_path, 'r') as file_read:
-        data = json.load(file_read)
-
-print(data)
-with open(storage_path, mode='r+') as file:
-    if args.val:
-        if args.val not in data.get(args.key):
-            data.get(args.key).append(args.val)
-        json.dump(data, file)
+if args.val:
+    if not os.path.isfile(storage_path):
+        with open(storage_path, 'w') as file_create:
+            data = {args.key: [args.val]}
+            json.dump(data, file_create)
     else:
-        data = json.load(file)
-        if None not in data.get(args.key, None):
-            print(', '.join(data.get(args.key)))
-        else:
-            print(None)
-
-
-
-# with open(storage_path, 'r') as f:
-#     if args.val:
-#         print(f'write {args.key=} and {args.val=} in the data file.')
-#         data = json.load(f)
-#         if data[args.key] in data:
-#             data[args.key] = data[args.key] + [args.val]
-#         else:
-#             data.update({args.key: [args.val]})
-#         json.dump(data, f)
-#     else:
-#         print(f'read {args.key=} from data file')
-#         data = json.load(f)
-#         if len(data[args.key]) > 1:
-#             print(', '.join(data.get(args.key)))
-#         else:
-#             print(*data.get(args.key))
+        with open(storage_path, 'r+') as file:
+            data = json.loads(file.read())
+            if args.key not in data:
+                data[args.key] = [args.val]
+            else:
+                data.get(args.key).append(args.val)
+    with open(storage_path, 'w') as file_save:
+        json.dump(data, file_save)
+else:
+    if not os.path.isfile(storage_path):
+        print(None)
+    else:
+        with open(storage_path, 'r') as file:
+            data = json.loads(file.read())
+            if args.key in data:
+                print(', '.join(data.get(args.key)))
+            else:
+                print(None)
