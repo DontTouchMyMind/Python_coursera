0003392468df81d1399cb5d81a3d6f56bab2e56b
DontTouchMyMind <tobigface@gmail.com>
2020-12-29 11:08:31 +0200

    Added solution from the coursera.

--- week_2/decorators.py
+++ week_2/decorators.py
@@ -8,3 +8,19 @@ def to_json(func):
         result = json.dumps(func(*args, **kwargs))
         return result
     return wrap
+
+
+# Solution from the teacher
+#
+# import functools
+# import json
+#
+#
+# def to_json(func):
+#
+#     @functools.wraps(func)
+#     def wrapped(*args, **kwargs):
+#         result = func(*args, **kwargs)
+#         return json.dumps(result)
+#
+#     return wrapped
